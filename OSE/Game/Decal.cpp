3160a66ff4f18afcee7b18bd4deae1a4496e872b
Hinton <hinton.oscar@gmail.com>
2012-06-20 16:36:17 +0200

    Renived SetSprite from decal.

--- OSE/Game/Decal.cpp
+++ OSE/Game/Decal.cpp
@@ -5,17 +5,10 @@ LINKCLASSTONAME("ent_decal", Decal);
 
 Decal::Decal(void)
 {
-	RegisterInput("SetSprite", SetSprite);
 	SetPos(Vector2::Random(-80.f,80.f));
 	
 	RenderInit();
 	SetDrawOrder(RENDERGROUP_ENTITIES);
-}
-
-void Decal::SetSprite(BaseObject* ent, VariantMap &Data)
-{
-	Decal* me = dynamic_cast<Decal*>(ent);
-	me->mPath = Data.ReadString("path");
 }
 
 void Decal::Spawn()
--- OSE/Game/Decal.h
+++ OSE/Game/Decal.h
@@ -4,9 +4,8 @@
 class Decal : public BaseObject
 {
 private:
-	std::string mPath;
+	
 public:
-	static void SetSprite(BaseObject* ent, VariantMap &Data);
 	Decal(void);
 	~Decal(void);
 	void Spawn();
