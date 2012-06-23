83fa97d58316824a845fd611ad4607c6e3f71bfd
Connor Charles <ccharles.gb@gmail.com>
2012-06-23 01:00:04 +0100

    XML support works

--- OSE/Engine/Utilities/XMLParser.h
+++ OSE/Engine/Utilities/XMLParser.h
@@ -7,17 +7,19 @@
 class ModelData
 {
 public:
-	ModelData() {mVertexCount = 0; mDensity = 0.f; mTexturePath = NULL;};
+	ModelData() {mVertexCount = 0; mDensity = 0.f; mTexturePath = NULL; mScale = 1.f;};
 	Vector2 mVertices[8];
 	int mVertexCount;
 	float mDensity;
+
 	const char* mTexturePath;
+	float mScale;
 };
 
 class XMLParser
 {
 private:
-
+	
 public:
 	static void LoadFile(const char *path);
 };
