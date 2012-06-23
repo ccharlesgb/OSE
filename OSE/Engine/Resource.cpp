b3cb6fabfd264a3236bb519a7ac9ceac6d4dd718
Hinton <hinton.oscar@gmail.com>
2012-06-23 02:06:42 +0200

    Documented Resource loader a bit.

--- OSE/Engine/Resource.cpp
+++ OSE/Engine/Resource.cpp
@@ -4,14 +4,27 @@
 std::map<std::string, sf::Texture*> Resource::TextureCache;
 sf::Texture* Resource::ErrorTex = NULL;
 
+/**
+ * Cleanup the resources.
+ *
+ * Loop all the resources and delete them.
+ */
 void Resource::Cleanup()
 {
 	std::cout << "CLEANING UP RESOURCES \n";
 	
+	std::cout << "CLEANING UP TEXTURES \n";
 	std::map<std::string, sf::Texture*>::iterator it;
 	for(it = TextureCache.begin(); it != TextureCache.end(); it++) {
 		std::cout << "DELETING " << it->first << "\n";
 		delete TextureCache[it->first];
+	}
+	
+	std::cout << "CLEANING UP SOUNDS \n";
+	std::map<std::string, sf::SoundBuffer*>::iterator SoundIterator;
+	for(SoundIterator = SoundCache.begin(); SoundIterator != SoundCache.end(); it++) {
+		std::cout << "DELETING " << SoundIterator->first << "\n";
+		delete SoundCache[SoundIterator->first];
 	}
 }
 
@@ -59,6 +72,11 @@ std::string Resource::GetSoundPath(const char* path)
 	return ("images/" + std::string(path) + ".wav").c_str();
 }
 
+/**
+ * Precache sound.
+ *
+ * Load the sound from the file path and insert the SoundBuffer into the SoundCache map.
+ */
 void Resource::PrecacheSound(const char* path)
 {
 	std::string FILE_PATH = GetSoundPath(path);
@@ -72,6 +90,12 @@ void Resource::PrecacheSound(const char* path)
 	SoundCache[path] = Buffer;
 }
 
+/**
+ * Get a sound file.
+ *
+ * If the sound is not already precached, precache it and then serve it,
+ * otherwise use the precached value.
+ */
 sf::Sound* Resource::RequestSound(const char* path)
 {
 	if (SoundCache[path] == NULL)
