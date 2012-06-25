cae3fc6dcece60700b61f6f825db211b975da8de
Connor Charles <ccharles.gb@gmail.com>
2012-06-25 01:01:48 +0100

    Added model

---
+++ OSE/Engine/Model.cpp
@@ -0,0 +1,9 @@
+#include "Model.h"
+
+Model::Model()
+{
+	mVertexCount = 0;
+	mDensity = 0.f;
+	mTexturePath = NULL;
+	mScale = 1.f;
+}
---
+++ OSE/Engine/Model.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Utilities/Vector2.h"
+
+class Model
+{
+public:
+	Model();
+	Vector2 mVertices[8];
+	int mVertexCount;
+	float mDensity;
+	const char* mTexturePath;
+	float mScale;
+};
+
